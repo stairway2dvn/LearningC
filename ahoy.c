5d80b816ed80f34014d4bfddf4373ad028b7a8e5
Devin Brooks <devincbrooks@gmail.com>
2011-09-22 20:38:52 -0500

    fun with C from cs50

---
+++ ahoy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int
+main(void)
+{
+    printf("Hello, what's your name?\n");
+    string(s)= GetString();
+    printf("Ahoy, %s, thar be land ahead!\n", s);
+    return 0;
+}
+
